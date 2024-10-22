79b26c9c88474c4b6abf746a8ae789ec93d8afcf
Nekroido <nekroido@windowslive.com>
2024-10-22 17:23:01 +0200

    Added README. Fixed solution file. Updated .gitignore. Added configuration files.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Game of Life
+
+A simple Game of Life implementation with F# and Garnet.
+
+## Prerequisites
+
+- [.NET Framework 8.0+](https://dotnet.microsoft.com/en-us/download/dotnet/8.0)
+
+Run the following commands to get started.
+
+```sh
+dotnet restore
+dotnet tool restore
+```
+
+## Running the game
+
+To run the game in console use the following command:
+
+```sh
+dotnet run --project src/Console/Console.fsproj <width> <height>
+```
--- src/Console/Program.fs
+++ src/Console/Program.fs
@@ -6,9 +6,9 @@ module App =
     [<EntryPoint>]
     let main args =
         let width, height =
-            args |> Seq.tryItem 0 |> Option.defaultValue "50" |> int,
-            args |> Seq.tryItem 1 |> Option.defaultValue "50" |> int
+            args |> Seq.tryItem 0 |> Option.defaultValue "25" |> int,
+            args |> Seq.tryItem 1 |> Option.defaultValue "25" |> int
 
-        let game = Game.Run(width, height, true)
+        Game.Run(width, height, true)
 
         0
